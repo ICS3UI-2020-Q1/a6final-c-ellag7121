18165da5edd429ade34650e5f9f8bf8f0dc7fb6f
ellag7121 <ellag7121@wrdsb.ca>
2020-10-09 12:18:19 +0000

    finished a6 final!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * get 5 numbers from the user that represent "cards", calculate the Hi-Lo count of those cards and tell it to the player
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //make the scanner to get user input
+    Scanner input = new Scanner(System.in);
+
+    //initialize variables
+    int count = 0; //the current card count (+1 if low, -1 if high)
+
+    //create the array
+    int[] cards = new int[5]; //this will hold the card values
+
+    //ask user for cards
+    System.out.println("Please enter the value of 5 cards:");
+    //make a loop to store cards in the array
+    for(int i = 0; i < cards.length; i++){
+      //put the user input in the array
+      cards[i] = input.nextInt();
+
+      //add or subtract from the count depending on the card
+      if(cards[i] >= 10 || cards[i] == 1){ //if it is a "high" card
+        //subtract 1 from the count
+        count--;
+      }else if(cards[i] <= 6){ //if it is a "low" card
+        //add 1 to the count
+        count++;
+      } //if it is not high or low, do nothing
+    }
+
+    //tell the user the card count
+    System.out.println("The count would be " + count);  
   }
 }
